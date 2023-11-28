cd653327e1607dd2d6ec78ed5ea9214e97bce775
c4arl0s <c.santiago.cruz@gmail.com>
2023-11-27 23:39:30 -0600

    Replace variable with an appropiate name

--- git-add-with-filter-ui.sh
+++ git-add-with-filter-ui.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-PARAMETER=$1
-UNTRACKED_FILES=$(git ls-files --others --exclude-standard | grep $PARAMETER)
-MODIFIED_FILES=$(git ls-files -m | grep $PARAMETER)
+EXTENSION=$1
+UNTRACKED_FILES=$(git ls-files --others --exclude-standard | grep $EXTENSION)
+MODIFIED_FILES=$(git ls-files -m | grep $EXTENSION)
 
-UNTRACKED_FILES_ERROR_MSG="Untracked files with filter: $PARAMETER don't exist"
-MODIFIED_FILES_ERROR_MSG="Modified files with filter: $PARAMETER don't exist"
+UNTRACKED_FILES_ERROR_MSG="Untracked files with extension $EXTENSION don't exist"
+MODIFIED_FILES_ERROR_MSG="Modified files with extension $EXTENSION don't exist"
 
 WARNING_UNTRACKED_MSG=
 WARNING_MODIFIED_MSG=
 
 if [[ $UNTRACKED_FILES ]]; then
     let COUNTER=0
-    LINE=$(git ls-files --others --exclude-standard | grep $PARAMETER | 
+    LINE=$(git ls-files --others --exclude-standard | grep $EXTENSION | 
            while read UNTRACKED_FILE
            do 
                let "COUNTER+=1"
                echo "\"$UNTRACKED_FILE\" \"$COUNTER\" off"
            done)
     echo $LINE;
-    SELECTED_UNTRACKED_FILES=$(echo $LINE | xargs dialog --stdout --checklist "untracked files to add with filter ($PARAMETER) :" 0 0 0)
+    SELECTED_UNTRACKED_FILES=$(echo $LINE | xargs dialog --stdout --checklist "untracked files to add with extension ($EXTENSION) :" 0 0 0)
     [ ! -z "$SELECTED_UNTRACKED_FILES" ] && echo $SELECTED_UNTRACKED_FILES | xargs git add || WARNING_UNTRACKED_MSG="🟡 You did not select any untracked file"
 else
     echo $UNTRACKED_FILES_ERROR_MSG
@@ -27,14 +27,14 @@ fi
 
 if [[ $MODIFIED_FILES ]]; then
     let COUNTER=0
-    LINE=$(git ls-files -m | grep $PARAMETER | 
+    LINE=$(git ls-files -m | grep $EXTENSION | 
            while read MODIFIED_FILE
            do 
                let "COUNTER+=1"
                echo "\"$MODIFIED_FILE\" \"$COUNTER\" off"
            done)
     echo $LINE
-    SELECTED_MODIFIED_FILES=$(echo $LINE | xargs dialog --stdout --checklist "modified files to add with filter ($PARAMETER):" 0 0 0)
+    SELECTED_MODIFIED_FILES=$(echo $LINE | xargs dialog --stdout --checklist "modified files to add with extension ($EXTENSION):" 0 0 0)
     [ ! -z "$SELECTED_MODIFIED_FILES" ] && echo $SELECTED_MODIFIED_FILES | xargs git add || WARNING_MODIFIED_MSG="🟡 You did not select any modified file"
 else
     echo $MODIFIED_FILES_ERROR_MSG
