b8282926e24d67407a6b0b1d57c79ef62d453c43
c4arl0s <c.santiago.cruz@gmail.com>
2023-11-27 23:32:02 -0600

    Send a warning message when you did not select any file

--- git-add-with-filter-ui.sh
+++ git-add-with-filter-ui.sh
@@ -7,6 +7,9 @@ MODIFIED_FILES=$(git ls-files -m | grep $PARAMETER)
 UNTRACKED_FILES_ERROR_MSG="Untracked files with filter: $PARAMETER don't exist"
 MODIFIED_FILES_ERROR_MSG="Modified files with filter: $PARAMETER don't exist"
 
+WARNING_UNTRACKED_MSG=
+WARNING_MODIFIED_MSG=
+
 if [[ $UNTRACKED_FILES ]]; then
     let COUNTER=0
     LINE=$(git ls-files --others --exclude-standard | grep $PARAMETER | 
@@ -17,9 +20,9 @@ if [[ $UNTRACKED_FILES ]]; then
            done)
     echo $LINE;
     SELECTED_UNTRACKED_FILES=$(echo $LINE | xargs dialog --stdout --checklist "untracked files to add with filter ($PARAMETER) :" 0 0 0)
-    echo $SELECTED_UNTRACKED_FILES | xargs git add
+    [ ! -z "$SELECTED_UNTRACKED_FILES" ] && echo $SELECTED_UNTRACKED_FILES | xargs git add || WARNING_UNTRACKED_MSG="🟡 You did not select any untracked file"
 else
-    echo -e $UNTRACKED_FILES_ERROR_MSG
+    echo $UNTRACKED_FILES_ERROR_MSG
 fi
 
 if [[ $MODIFIED_FILES ]]; then
@@ -32,7 +35,10 @@ if [[ $MODIFIED_FILES ]]; then
            done)
     echo $LINE
     SELECTED_MODIFIED_FILES=$(echo $LINE | xargs dialog --stdout --checklist "modified files to add with filter ($PARAMETER):" 0 0 0)
-    echo $SELECTED_MODIFIED_FILES | xargs git add
+    [ ! -z "$SELECTED_MODIFIED_FILES" ] && echo $SELECTED_MODIFIED_FILES | xargs git add || WARNING_MODIFIED_MSG="🟡 You did not select any modified file"
 else
-    echo -e $MODIFIED_FILES_ERROR_MSG
+    echo $MODIFIED_FILES_ERROR_MSG
 fi
+
+[ ! -z "$WARNING_UNTRACKED_MSG" ] && echo $WARNING_UNTRACKED_MSG
+[ ! -z "$WARNING_MODIFIED_MSG" ] && echo $WARNING_MODIFIED_MSG
